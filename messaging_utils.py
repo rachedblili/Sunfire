dac5dab4f14413fcf331f8a7ec3817833e01d077
Rached Blili <rached@sedonasys.com>
2024-05-26 22:59:25 -0700

    switched to thread-safe Queue for SSE management

--- messaging_utils.py
+++ messaging_utils.py
@@ -1,15 +1,15 @@
 import time
-
+from queue import Queue
 # Initialize the message queue
-message_queue = []
+message_queue = Queue()
 
 
 def message_manager():
     """Generator function to yield progress messages."""
     global message_queue
     while True:
-        if message_queue:
-            (facility, message) = message_queue.pop(0)
+        if not message_queue.empty():
+            facility, message = message_queue.get()
             yield f"data: {facility} : {message}\n\n"
         time.sleep(1)
 
@@ -17,4 +17,4 @@ def message_manager():
 def logger(facility, message):
     """Append a new message to the queue."""
     global message_queue
-    message_queue.append((facility, message))
+    message_queue.put((facility, message))
