c91bb0a79478d20285c68f99ae056d8e710b8cce
Rached Blili <rached@sedonasys.com>
2024-06-21 16:06:46 -0700

    Added keepalive on SSE channel

--- messaging_utils.py
+++ messaging_utils.py
@@ -8,11 +8,17 @@ message_queue = Queue()
 def message_manager():
     """Generator function to yield progress messages."""
     global message_queue
+    count = 0
     while True:
         if not message_queue.empty():
             session_id, facility, message = message_queue.get()
             yield f"data: {session_id} : {facility} : {message}\n\n"
             sys.stdout.flush()
+        elif count >= 10:
+            yield f"data: xxx : keepalive : ping\n\n"
+            sys.stdout.flush()
+            count = 0
+        count += 1
         time.sleep(1)
 
 
